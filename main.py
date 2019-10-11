dc6701fe0ba010ab0a5a693f07ed41edba1a09ff
thaycacac <thaycacac@gmail.com>
2019-10-11 18:07:12 +0700

    fix: bug

--- main.py
+++ main.py
@@ -6,75 +6,83 @@ screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("A Maze Game")
 screen.setup(700, 700)
+screen.tracer(0)
 
-
-treasures = []
 walls = []
-levels = []
+treasures = []
 
 class Pen(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("square")
-        
         self.color("white")
         self.penup()
         self.speed(0)
 
-class Treasure(turtle.Turtle):
-    def __init__(self, x, y):
-        turtle.Turtle.__init__(self)
-        self.shape("square")
-        self.color("yellow")
-        self.penup()
-        self.speed(0)
-        self.goto(x, y)
-
 class Player(turtle.Turtle):
+    
     def __init__(self):
         turtle.Turtle.__init__(self)
-        self.shape("turtle")
-        self.color("yellow")
+        self.shape("square")
+        self.color("blue")
         self.penup()
         self.speed(0)
-        self.hp=100
+        self.gold = 0
         
     def up(self):
-        x=self.xcor()
-        y=self.ycor()
-        if (x,y+24) not in walls:
-            self.goto(x,y+24)
-            
+        move_to_x = self.xcor()
+        move_to_y = self.ycor() + 24
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
     def down(self):
-        x=self.xcor()
-        y=self.ycor()
-        if (x,y-24) not in walls:
-            self.goto(x,y-24)
-
-              
-    def right(self):
-        x=self.xcor()
-        y=self.ycor()
-        if (x+24,y) not in walls:
-            self.goto(x+24,y)
-
+        move_to_x = self.xcor()
+        move_to_y = self.ycor() - 24
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
     def left(self):
-        x=self.xcor()
-        y=self.ycor()
-        if (x-24,y) not in walls:
-            self.goto(x-24,y)
-            
-    def is_collission(self,other):
-        if self.xcor() == other.xcor() & self.ycor() == other.ycor():
+        move_to_x = self.xcor() - 24
+        move_to_y = self.ycor()
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
+    def right(self):
+        move_to_x = self.xcor() + 24
+        move_to_y = self.ycor()
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def is_collision(self, other):
+        a = self.xcor() - other.xcor()
+        b = self.ycor() - other.ycor()
+        distance = math.sqrt((a ** 2) + (b ** 2))
+        if distance < 5:
             return True
         else:
             return False
-         
-level_1 = [ 
+
+class Treasure(turtle.Turtle):
+
+    def __init__(self, x, y):
+        turtle.Turtle.__init__(self)
+        self.shape("circle")
+        self.color("yellow")
+        self.penup()
+        self.speed(0)
+        self.gold = 100
+        self.goto(x, y)
+
+    def destroy(self):
+        self.goto(2000, 2000)
+        self.hideturtle()
+
+
+level = [
 "XXXXXXXXXXXXXXXXXXXXXXXXX",
 "XP  XXXXXXXE         XXXX",
 "X  XXXXXXX  XXXXXX  XXXXX",
-"X       XX  XXXXXXT XXXXX",
+"X       XX  XXXXXX  XXXXX",
 "X      XX  XXX        XXX",
 "XXXXXX  XX  XXX        XX",
 "XXXXXX  XX  XXXXXX  XXXXX",
@@ -82,23 +90,24 @@ level_1 = [
 "X  XXX        XXXX  XXXXX",
 "X  XXX  XXXXXXXXXXXXXXXXX",
 "X         XXXXXXXXXXXXXXX",
-
 "XE               XXXXXXXX",
 "XXXXXXXXXXXX     XXXXX  X",
 "XXXXXXXXXXXXXXX  XXXXX  X",
 "XXX  XXXXXXXXXX         X",
 "XXX                     X",
-"XXX         TXXXXXXXXXXXX",
+"XXX        TXXXXXXXXXXXXX",
 "XXXXXXXXXX  XXXXXXXXXXXXX",
 "XXXXXXXXXXE             X",
-"XX T XXXXX              X",
+"XX   XXXXX              X",
 "XX   XXXXXXXXXXXXX  XXXXX",
 "XX    XXXXXXXXXXXX  XXXXX",
 "XX     E   XXXX        XX",
-"XXXX                    X",
+"XXXX               T    X",
 "XXXXXXXXXXXXXXXXXXXXXXXXX"
 ]
 
+
+#setup level
 def setup_maze(level):
     for y in range(len(level)):
         for x in range(len(level[y])):
@@ -110,22 +119,27 @@ def setup_maze(level):
                 pen.stamp()
                 walls.append((position_x, position_y))
             elif character == "P":
-                player.goto((position_x, position_y))
-            elif character == "T" :
+                player.goto(position_x, position_y)
+            elif character == "T":
                 treasures.append(Treasure(position_x, position_y))
 
 pen = Pen()
 player = Player()
 
 turtle.listen()
-turtle.onkey(player.up,"Up")
-turtle.onkey(player.down,"Down")
-turtle.onkey(player.right,"Right")
-turtle.onkey(player.left,"Left")
+turtle.onkey(player.up, "Up")
+turtle.onkey(player.down, "Down")
+turtle.onkey(player.right, "Right")
+turtle.onkey(player.left, "Left")
 
-setup_maze(level_1)
+setup_maze(level)
+
 
 while True:
     for treasure in treasures:
-        if player.is_collission(treasure):
-            print("co linh")
+        if player.is_collision(treasure):
+            player.gold += treasure.gold
+            print('Play gold: {}'.format(player.gold))
+            treasure.destroy()
+            treasures.remove(treasure)
+    screen.update()
