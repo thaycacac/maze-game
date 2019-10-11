4650160afae46fd2c237365908e5be285e87f1e5
thaycacac | phatto pham <thaycacac@gmail.com>
2019-10-11 20:21:06 +0700

    update: treasure

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 walls = []
 treasures = []
 
+
 class Pen(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -19,34 +20,35 @@ class Pen(turtle.Turtle):
         self.penup()
         self.speed(0)
 
+
 class Player(turtle.Turtle):
-    
+
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("square")
         self.color("blue")
         self.penup()
         self.speed(0)
-        self.gold = 0
-        
+        self.hp = 0
+
     def up(self):
         move_to_x = self.xcor()
         move_to_y = self.ycor() + 24
         if (move_to_x, move_to_y) not in walls:
             self.goto(move_to_x, move_to_y)
-        
+
     def down(self):
         move_to_x = self.xcor()
         move_to_y = self.ycor() - 24
         if (move_to_x, move_to_y) not in walls:
             self.goto(move_to_x, move_to_y)
-        
+
     def left(self):
         move_to_x = self.xcor() - 24
         move_to_y = self.ycor()
         if (move_to_x, move_to_y) not in walls:
             self.goto(move_to_x, move_to_y)
-        
+
     def right(self):
         move_to_x = self.xcor() + 24
         move_to_y = self.ycor()
@@ -54,13 +56,11 @@ class Player(turtle.Turtle):
             self.goto(move_to_x, move_to_y)
 
     def is_collision(self, other):
-        a = self.xcor() - other.xcor()
-        b = self.ycor() - other.ycor()
-        distance = math.sqrt((a ** 2) + (b ** 2))
-        if distance < 5:
+        if self.xcor() == other.xcor() and self.ycor() == other.ycor():
             return True
         else:
             return False
+
 
 class Treasure(turtle.Turtle):
 
@@ -70,44 +70,44 @@ class Treasure(turtle.Turtle):
         self.color("yellow")
         self.penup()
         self.speed(0)
-        self.gold = 100
+        self.hp = 100
         self.goto(x, y)
 
     def destroy(self):
-        self.goto(2000, 2000)
+        self.goto(1000, 1000)
         self.hideturtle()
 
 
 level = [
-"XXXXXXXXXXXXXXXXXXXXXXXXX",
-"XP  XXXXXXXE         XXXX",
-"X  XXXXXXX  XXXXXX  XXXXX",
-"X       XX  XXXXXX  XXXXX",
-"X      XX  XXX        XXX",
-"XXXXXX  XX  XXX        XX",
-"XXXXXX  XX  XXXXXX  XXXXX",
-"XXXXXX  XX    XXXX  XXXXX",
-"X  XXX        XXXX  XXXXX",
-"X  XXX  XXXXXXXXXXXXXXXXX",
-"X         XXXXXXXXXXXXXXX",
-"XE               XXXXXXXX",
-"XXXXXXXXXXXX     XXXXX  X",
-"XXXXXXXXXXXXXXX  XXXXX  X",
-"XXX  XXXXXXXXXX         X",
-"XXX                     X",
-"XXX        TXXXXXXXXXXXXX",
-"XXXXXXXXXX  XXXXXXXXXXXXX",
-"XXXXXXXXXXE             X",
-"XX   XXXXX              X",
-"XX   XXXXXXXXXXXXX  XXXXX",
-"XX    XXXXXXXXXXXX  XXXXX",
-"XX     E   XXXX        XX",
-"XXXX               T    X",
-"XXXXXXXXXXXXXXXXXXXXXXXXX"
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XP  XXXXXXXE         XXXX",
+    "X  XXXXXXX  XXXXXX  XXXXX",
+    "X       TX  XXXXXX  XXXXX",
+    "X      XX  XXX        XXX",
+    "XXXXXX  XX  XXX        XX",
+    "XXXXXX  XX  XXXXXX  XXXXX",
+    "XXXXXX  XX    XXXX  XXXXX",
+    "X  XXX        XXXX  XXXXX",
+    "X  XXX  XXXXXXXXXXXXXXXXX",
+    "X         XXXXXXXXXXXXXXX",
+    "XE               XXXXXXXX",
+    "XXXXXXXXXXXX     XXXXX  X",
+    "XXXXXXXXXXXXXXX  XXXXX  X",
+    "XXX  XXXXXXXXXX         X",
+    "XXX                     X",
+    "XXX        TXXXXXXXXXXXXX",
+    "XXXXXXXXXX  XXXXXXXXXXXXX",
+    "XXXXXXXXXXE             X",
+    "XX   XXXXX              X",
+    "XX   XXXXXXXXXXXXX  XXXXX",
+    "XX    XXXXXXXXXXXX  XXXXX",
+    "XX     E   XXXX        XX",
+    "XXXX               T    X",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX"
 ]
 
 
-#setup level
+# setup level
 def setup_maze(level):
     for y in range(len(level)):
         for x in range(len(level[y])):
@@ -122,6 +122,7 @@ def setup_maze(level):
                 player.goto(position_x, position_y)
             elif character == "T":
                 treasures.append(Treasure(position_x, position_y))
+
 
 pen = Pen()
 player = Player()
@@ -138,8 +139,8 @@ setup_maze(level)
 while True:
     for treasure in treasures:
         if player.is_collision(treasure):
-            player.gold += treasure.gold
-            print('Play gold: {}'.format(player.gold))
+            player.hp += treasure.hp
+            print('Play gold: {}'.format(player.hp))
             treasure.destroy()
             treasures.remove(treasure)
     screen.update()
